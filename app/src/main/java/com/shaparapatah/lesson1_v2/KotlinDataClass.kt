5da3e50813b2ca91746056f5d3c2ed9524e49527
Shaparapatah <mm.fedorchenko@gmail.com>
2021-09-01 21:45:24 +0300

    Initial commit

---
+++ app/src/main/java/com/shaparapatah/lesson1_v2/KotlinDataClass.kt
@@ -0,0 +1,6 @@
+package com.shaparapatah.lesson1_v2
+
+data class KotlinDataClass(
+    var field1: String,
+    var field2: String,
+)
---
+++ app/src/main/java/com/shaparapatah/lesson1_v2/KotlinSingleton.kt
@@ -0,0 +1,4 @@
+package com.shaparapatah.lesson1_v2
+
+class KotlinSingleton {
+}
